c45cd0fa576a2e42db7a84890259d6fb21141f5f
earl <josepheaorle@gmail.com>
2015-03-08 18:50:23 -0500

    changed file extensions to php and pointed menu paths to those files

---
+++ atm/contact.php
@@ -0,0 +1,105 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><head>
+<link rel="icon" type="image/x-icon" href="images/favicon.ico" />
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Andreas Transo :: Traditional Irish Scottish English American Music</title>
+<link type="text/css" rel="stylesheet" href="stylesheet.css">
+<style type="text/css">
+a:link {
+	color: #030;
+}
+</style>
+    <script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js?ver=1.3.2'></script>
+    <script type='text/javascript' src='js/jquery.color-RGBa-patch.js'></script>
+    <script type='text/javascript' src='js/example.js'></script>
+    <script src="Scripts/swfobject_modified.js" type="text/javascript"></script>
+</head><body>
+<br>
+<div align="center"><img src="images/top.png" border="0"></div>
+<table class="main" align="center"><tbody><tr>
+  <td valign="top">
+<table cellspacing="10" width="100%"><tbody><tr align="center">
+  <td colspan="2"><object id="FlashID" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="851" height="189">
+    <param name="movie" value="andreas.swf" />
+    <param name="quality" value="high" />  
+	<param name="loop" value="false" />
+  	<param name="wmode" value="opaque" />
+    <param name="swfversion" value="6.0.65.0" />
+    <!-- This param tag prompts users with Flash Player 6.0 r65 and higher to download the latest version of Flash Player. Delete it if you don’t want users to see the prompt. -->
+    <param name="expressinstall" value="Scripts/expressInstall.swf" />
+    <!-- Next object tag is for non-IE browsers. So hide it from IE using IECC. -->
+    <!--[if !IE]>-->
+    <object type="application/x-shockwave-flash" data="andreas.swf" width="851" height="189">
+      <!--<![endif]-->
+      <param name="quality" value="high" />
+	  <param name="loop" value="false" />
+      <param name="wmode" value="opaque" />
+      <param name="swfversion" value="6.0.65.0" />
+      <param name="expressinstall" value="Scripts/expressInstall.swf" />
+      <!-- The browser displays the following alternative content for users with Flash Player 6.0 and older. -->
+      <div>
+        <h4>Content on this page requires a newer version of Adobe Flash Player.</h4>
+        <p><a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash player" width="112" height="33" /></a></p>
+      </div>
+      <!--[if !IE]>-->
+    </object>
+    <!--<![endif]-->
+  </object>
+     <div class="nav-wrap">
+
+  		<?php
+        include 'menu.php';
+      ?>
+
+    </div></td></tr>
+  <tr align="center">
+    <td width="466" valign="top">
+    <div class="sidebar">
+    
+        <div class="h4">tØ book</div>
+
+<p class="normal"><strong><img src="images/hgh.jpg" width="150" height="199" hspace="10" vspace="10" align="right" />TO BOOK EITHER ANDREAS TRANSØ <br />
+  OR HANDFUL OF EARTH, CONTACT: </strong><br />
+  <br />
+  608.485.3898<br />
+  <a href="mailto:andreastranso@gmail.com">andreastranso@gmail.com</a></p>
+<p class="normal"><strong>PRESS</strong><br />
+  To download high-resolution press<br />
+  images <a href="/TransoPress.zip">click here</a>.</p>
+<p class="normal">To download high-resolution show flyer,<br />
+  <a href="/flyer.pdf">click here</a>.</p>
+    </div>
+    <div class="sidebar">
+      
+      <div class="h4">WHAT HE PLAYS</div>
+
+<p class="normal"><strong>Instrumentation...</strong></p>
+<ul>
+  <li>19'' Brock Robinson bodhrán (Green Bay, Wisconsin maker) </li>
+  <li>Paul Hathway mandola (London, England maker) </li>
+  <li>Rob Forkner's <a href="www.metloef.com">Metloef</a> bodhráns </li>
+  <li>Late '70s/early '80s Yamaha FG-375 SII guitar (formerly Steve Twigger's of <a href="www.gaelicstorm.com">Gaelic Storm</a>) </li>
+  <li>1994 <a href="www.bischoffguitars.com">Gordon Bischoff</a> 2JR-078 guitar (Eau Claire, Wisconsin luthier) </li>
+  <li><a href="www.willkilkeary.com">Will Kilkeary</a> bones (a LaFarge, Wisconsin maker) </li>
+  <li>Tin whistles...any that survive the road </li>
+  <li>Various spoons, preferably of the soup variety </li>
+  <li>His face (yes, it too can be an instrument)</li>
+</ul>
+<p class="normal"><img src="images/nhfgb.jpg" width="466" height="203" /></p>
+    </div>
+    <p><img style="visibility:hidden;width:0px;height:0px;" border=0 width=0 height=0 src="http://c.gigcount.com/wildfire/IMP/CXNID=2000002.0NXC/bT*xJmx*PTEyOTQ2MjE*NzM5MjQmcHQ9MTI5NDYyMTQ3OTk4NyZwPTI3MDgxJmQ9cHJvX3NjaGVkdWxlX2ZpcnN*X2dlbiZnPTEm/bz*yN2YxYWIzMWEwYjQ*ODY1ODc2MWExMTdiMWUyMDVkMSZvZj*w.gif" /><br /><img style="visibility:hidden;width:0px;height:0px;" border=0 width=0 height=0 src="http://www.reverbnation.com/widgets/trk/42/artist_1225350//t.gif" /></p></td><td valign="top" width="321">        
+
+ <div class="sidebar"><div class="h4">The Buzz</div><img style="visibility:hidden;width:0px;height:0px;" border=0 width=0 height=0 src="http://c.gigcount.com/wildfire/IMP/CXNID=2000002.0NXC/bT*xJmx*PTEyOTQ2MjM*NzE1MzgmcHQ9MTI5NDYyMzQ3Mzk*NSZwPTI3MDgxJmQ9cHJvX3ByZXNzX2ZpcnN*X2dlbiZnPTEmbz*y/N2YxYWIzMWEwYjQ*ODY1ODc2MWExMTdiMWUyMDVkMSZvZj*w.gif" /><embed type="application/x-shockwave-flash" src="http://cache.reverbnation.com/widgets/swf/43/pro_widget.swf" height="200" width="320" align="top" bgcolor="#ffffff" loop="false" wmode="transparent" quality="best" allowScriptAccess="always" allowNetworking="all" allowFullScreen="true" seamlesstabbing="false" flashvars="id=artist_1225350&posted_by=&skin_id=PWPS4008&font_color=454A45" /><br /></div>
+ <div class="sidebar"><div class="h4">News &amp; Such</div><iframe src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fpages%2FAndreas-Transo%2F192237597453890%3Fv%3Dapp_2405167945%26ref%3Dts%23%21%2Fpages%2FAndreas-Transo%2F192237597453890%3Fv%3Dwall&amp;width=320&amp;colorscheme=light&amp;show_faces=true&amp;stream=true&amp;header=false&amp;height=480" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:320px; height:480px;" allowTransparency="true"></iframe>
+</div></td></tr><tr bgcolor="#ffcc99" valign="middle">
+  <td colspan="2" align="center" bgcolor="#000000" height="40">&nbsp;</td></tr></tbody></table>
+</td></tr></tbody></table>
+<div align="center"><img src="images/bottom.png" border="0"></div>
+<script type="text/javascript">
+swfobject.registerObject("FlashID");
+</script>
+</body><script type="text/javascript">
+  Meebo("domReady");
+</script>
+</html>
--- atm/menu.php
+++ atm/menu.php
@@ -1,9 +1,9 @@
 <ul class="group" id="example-one">
-  <li><a href="index">Home</a></li> 
-  <li><a href="handfulofearth">Handful of Earth</a></li>            
-  <li><a href="bio">BiO</a></li>
-  <li><a href="music">Music</a></li>
-  <li><a href="events">Events</a></li>
-  <li><a href="contact">Contact</a></li>
-  <li><a href="links">Links</a></li>
+  <li><a href="index.php">Home</a></li> 
+  <li><a href="handfulofearth.php">Handful of Earth</a></li>            
+  <li><a href="bio.php">BiO</a></li>
+  <li><a href="music.php">Music</a></li>
+  <li><a href="events.php">Events</a></li>
+  <li><a href="contact.php">Contact</a></li>
+  <li><a href="links.php">Links</a></li>
 </ul>
