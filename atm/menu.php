1c72831f2dd21804f8a16463eadfd08d7f593372
earl <josepheaorle@gmail.com>
2015-03-08 17:23:32 -0500

    included menu via php script

---
+++ atm/menu.php
@@ -0,0 +1,9 @@
+<ul class="group" id="example-one">
+  <li><a href="index">Home</a></li> 
+  <li><a href="handfulofearth">Handful of Earth</a></li>            
+  <li><a href="bio">BiO</a></li>
+  <li><a href="music">Music</a></li>
+  <li><a href="events">Events</a></li>
+  <li><a href="contact">Contact</a></li>
+  <li><a href="links">Links</a></li>
+</ul>
